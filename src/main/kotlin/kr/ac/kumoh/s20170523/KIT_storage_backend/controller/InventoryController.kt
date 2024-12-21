6d15f1206cc1de46ad7199e009ce348a3458fcaa
heehong <gmlghd98@gmail.com>
2024-12-21 18:15:43 +0900

    🚀 초기 Backend 완성

--- src/main/kotlin/kr/ac/kumoh/s20170523/KIT_storage_backend/controller/InventoryController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20170523/KIT_storage_backend/controller/InventoryController.kt
@@ -2,11 +2,31 @@ package kr.ac.kumoh.s20170523.KIT_storage_backend.controller
 
 import kr.ac.kumoh.s20170523.KIT_storage_backend.model.Inventory
 import kr.ac.kumoh.s20170523.KIT_storage_backend.service.InventoryService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/inventories")
 class InventoryController(private val service:InventoryService) {
-    @GetMapping("api/inventories")
+    @GetMapping
     fun getAllInventories(): List<Inventory> = service.getAllInventories()
+
+    @GetMapping("/{id}")
+    fun getInventoryById(@PathVariable id:String): Inventory? = service.getInventoryById(id)
+
+    @GetMapping("/user/{user}")
+    fun getInventoryByUser(@PathVariable user: String): List<Inventory> = service.getInventoryByUser(user)
+
+    @PostMapping
+    fun addInventory(@RequestBody inventory: Inventory): Inventory = service.addInventory(inventory)
+
+    @PutMapping("/{id}")
+    fun updateInventory(@PathVariable id: String, @RequestBody inventoryDetails: Inventory): Inventory? =  service.updateInventory(id, inventoryDetails)
+
+    @DeleteMapping("/{id}")
+    fun deleteInventory(@PathVariable id: String): Map<String, String> {
+        return if (service.deleteInventory(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
--- src/main/kotlin/kr/ac/kumoh/s20170523/KIT_storage_backend/service/InventoryService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20170523/KIT_storage_backend/service/InventoryService.kt
@@ -6,5 +6,38 @@ import org.springframework.stereotype.Service
 
 @Service
 class InventoryService(private val repository: InventoryRepository) {
+    fun addInventory(inventory: Inventory): Inventory = repository.save(inventory)
+
     fun getAllInventories(): List<Inventory> = repository.findAll()
+    fun getInventoryById(id: String): Inventory? = repository.findById(id).orElse(null)
+    fun getInventoryByUser(user: String): List<Inventory> = repository.findByUser(user)
+
+    fun updateInventory(id: String, inventory: Inventory): Inventory? {
+        val inventoryTarget = repository.findById(id)
+
+        return if (inventoryTarget.isPresent) {
+            val oldSong = inventoryTarget.get()
+            val updatedSong = oldSong.copy(
+                image = inventory.image,
+                name = inventory.name,
+                user = inventory.user,
+                quantity = inventory.quantity,
+                startDate = inventory.startDate,
+                endDate = inventory.endDate,
+            )
+            repository.save(updatedSong)
+        } else {
+            null
+        }
+    }
+
+    fun deleteInventory(id: String): Boolean {
+        return if (repository.existsById(id)) {
+            repository.deleteById(id)
+            true
+        } else {
+            false
+        }
+    }
+
 }
