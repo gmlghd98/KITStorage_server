bb16a17ef0d5f43304a7fae7415ac2734f0c7fe0
heehong <gmlghd98@gmail.com>
2024-12-21 17:05:32 +0900

    🎉 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20170523/KIT_storage_backend/KitStorageBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20170523.KIT_storage_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KitStorageBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
